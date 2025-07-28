a25e451879d948bfd1520ee2951fce03cfaba123
Yusuf <mdyusuf2521@gmail.com>
2025-07-28 10:07:36 +0530

    client setup

---
+++ client/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="">
+         <Link href="/stream">
+          Stream
+         </Link>
+         <Link href={"/watch"}>
+          Watch
+         </Link>
+    </div>
+  );
+}
