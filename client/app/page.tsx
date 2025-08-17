9d0baaae8696f775e59a2aa1db516eece0ee8b26
Yusuf <mdyusuf2521@gmail.com>
2025-08-17 21:01:48 +0530

    beautified home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,12 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="min-h-screen w-full flex items-center justify-center gap-6">
          <Link href="/stream">
+          <span className="bg-red-500 text-white p-2 rounded-sm">
           Stream
+          </span>
          </Link>
          <Link href={"/watch"}>
+         <span className="bg-blue-500 text-white p-2 rounded-sm">
           Watch
+         </span>
          </Link>
     </div>
   );
